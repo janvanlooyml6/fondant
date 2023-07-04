7b89a5e726b8a3a5dcc19d20b474089b7d3f3a2f
Philippe Moussalli <philippe.moussalli95@gmail.com>
2023-07-04 14:58:02 +0200

    enable-building-specific-reusable-components

--- scripts/tag_components.sh
+++ scripts/tag_components.sh
@@ -6,6 +6,8 @@ function usage {
   echo "Options:"
   echo "  -o, --old-tag <value>    Old tag to of image to retag"
   echo "  -n, --new-tag <value>    New tag to tag image with"
+  echo "  -co, --component <value>  Set the component name. Pass the component folder name to build
+  a certain components or 'all' to build all components in the components directory (default: all)"
   echo "  -h, --help               Display this help message"
 }
 
@@ -13,6 +15,7 @@ function usage {
 while [[ "$#" -gt 0 ]]; do case $1 in
   -o|--old-tag) old_tag="$2"; shift;;
   -n|--new-tag) new_tag="$2"; shift;;
+  -co|--component) component="$2"; shift;;
   -h|--help) usage; exit;;
   *) echo "Unknown parameter passed: $1"; exit 1;;
 esac; shift; done
@@ -22,6 +25,11 @@ if [ -z "${old_tag}" ] || [ -z "${new_tag}" ]; then
   echo "Error: missing parameter"
   usage
   exit 1
+fi
+
+# Set default values for optional arguments if not passed
+if [ -z "${component-}" ]; then
+    component="all"
 fi
 
 # Set github repo information
@@ -38,14 +46,17 @@ for dir in "$component_dir"/*/; do
 
   BASENAME=${dir%/}
   BASENAME=${BASENAME##*/}
-  old_image_name=ghcr.io/${namespace}/${BASENAME}:${old_tag}
-  new_image_name=ghcr.io/${namespace}/${BASENAME}:${new_tag}
-  echo "$old_image_name"
-  echo "$new_image_name"
+  if [[ "$BASENAME" == "${component}" ]] || [[ "${component}" == "all" ]]; then
 
-  docker pull "$old_image_name"
-  docker tag "$old_image_name" "$new_image_name"
-  docker push "$new_image_name"
+    old_image_name=ghcr.io/${namespace}/${BASENAME}:${old_tag}
+    new_image_name=ghcr.io/${namespace}/${BASENAME}:${new_tag}
+    echo "$old_image_name"
+    echo "$new_image_name"
 
-  popd
+    docker pull "$old_image_name"
+    docker tag "$old_image_name" "$new_image_name"
+    docker push "$new_image_name"
+
+    popd
+  fi
 done
