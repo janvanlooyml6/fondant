1346b6c0bc9dce72b209f3649558cf1d3d7a10aa
Sharon Grundmann <sharon.grundmann@ml6.eu>
2023-08-23 17:56:08 +0200

    Add index filtering

--- components/common_crawl_download/src/main.py
+++ components/common_crawl_download/src/main.py
@@ -1,28 +1,80 @@
 """A component that downloads common crawl files."""
 import logging
-from typing import List
+from typing import List, Optional
 
 import dask.dataframe as dd
+import pyarrow.dataset as ds
 from fondant.component import DaskLoadComponent
 from trafilatura.settings import use_config
-from utils import extract_bytes_from_warc_file_http, extract_html
+from utils import (
+    extract_bytes_from_warc_file_http,
+    extract_html,
+    validate_commoncrawl_index_filters,
+)
 
 logger = logging.getLogger(__name__)
 
+CC_INDEX_PATH = "s3://commoncrawl/cc-index/table/cc-main/warc/"
+CC_BASE_URL = "s3://"
 
 class CommonCrawlDownloadComponent(DaskLoadComponent):
     """Component that download common crawl files."""
 
-    def __init__(self, *_, filters: List[str], extract_plain_text: bool):
+    def __init__(self,
+                 *_,
+                 filters: List,
+                 extract_plain_text: bool,
+                 n_records_to_download: Optional[int] = None):
         # init global trafilatura config for multi-processing
         self.trafilatura_config = use_config()
         self.trafilatura_config.set("DEFAULT", "EXTRACTION_TIMEOUT", "0")
-        self.filters = filters
+        self.filters = validate_commoncrawl_index_filters(filters)
         self.extract_plain_text = extract_plain_text
+        self.n_records_to_download = n_records_to_download
+
+
+    def filter_common_crawl_index(self) -> dd.DataFrame:
+        """Use common crawl index and provided filters to retrieve relevant pages
+        from common crawl.
+        """
+        logger.info("Filtering common crawl index...")
+        output_columns = [
+            "url_surtkey",
+            "url",
+            "warc_filename",
+            "warc_record_offset",
+            "warc_record_length",
+        ]
+
+        cc_index = ds.dataset(CC_INDEX_PATH, format="parquet", partitioning="hive")
+        warc_files = cc_index.files
+        warc_files = [CC_BASE_URL + file for file in warc_files] # TODO: change to http
+        warc_ddf = dd.read_parquet(
+            warc_files,
+            engine='pyarrow',
+            filters=self.filters,
+            columns=output_columns,
+        )
+
+        if self.n_records_to_download is not None:
+            warc_ddf = warc_ddf.head(self.n_records_to_download)
+
+        return warc_ddf
+
+
+    def download_warc_content(self, dataframe: dd.DataFrame) -> str:
+        """Download the content of the warc files."""
+        logger.info("Downloading common crawl files...")
+        return dataframe.assign(content=dataframe.apply(
+            lambda row: extract_bytes_from_warc_file_http(row["warc_filename"],
+                                                            row["warc_record_offset"],
+                                                            row["warc_record_length"]),
+            axis=1,
+            ),
+        )
 
     def load(self) -> dd.DataFrame:
         """
-
         Args:
             dataframe: Pandas dataframe.
 
@@ -37,18 +89,12 @@ class CommonCrawlDownloadComponent(DaskLoadComponent):
             ddf["url_content"] = ddf["url_content"].apply(
                 lambda x: extract_html(x, self.trafilatura_config), meta=("url_content", "str"))
 
-        return ddf
+        ddf = ddf.dropna(subset=["content"])
+        result_ddf = ddf[['url', 'content']]
 
-    def filter_common_crawl_index(self) -> dd.DataFrame:
-        """Use common crawl index and provided filters to retrieve relevant pages
-        from common crawl.
-        """
-        # TODO implementation by @shayorshay
-        # final dataframe should contain: url(index), warc_path, warc_offset, warc_length
+        result_ddf.columns = [
+            "webpage_url",
+            "webpage_content",
+        ]
 
-    def download_warc_content(self, dataframe: dd.DataFrame) -> str:
-        return dataframe.assign(content=dataframe.apply(
-            lambda row: extract_bytes_from_warc_file_http(row["warc_path"], row["warc_offset"],
-                                                          row["warc_length"]),
-            axis=1,
-            meta=("content", "str")))
+        return result_ddf
--- components/common_crawl_download/src/utils.py
+++ components/common_crawl_download/src/utils.py
@@ -1,16 +1,22 @@
+import logging
 from configparser import ConfigParser
 from io import BytesIO
+from typing import List
 
 import httpx
 import trafilatura
+from configs import VALID_COMMONCRAWL_INDEX_FILTERS, VALID_OPERATORS
 from warcio.archiveiterator import ArchiveIterator
 
+logger = logging.getLogger(__name__)
 
-def extract_bytes_from_warc_file_http(warc_filename: str, record_offset: int, record_length: int,
+def extract_bytes_from_warc_file_http(warc_filename: str,
+                                      warc_record_offset: int,
+                                      warc_record_length: int,
                                       prefix: str = "https://data.commoncrawl.org/"):
     """Extracts a specified range of bytes from a WARC file over HTTP."""
     url = f"{prefix}{warc_filename}"
-    headers = {"Range": f"bytes={record_offset}-{record_offset + record_length - 1}"}
+    headers = {"Range": f"bytes={warc_record_offset}-{warc_record_offset + warc_record_length - 1}"}
 
     with httpx.Client() as client:
         response = client.get(url, headers=headers)
@@ -24,6 +30,7 @@ def extract_bytes_from_warc_file_http(warc_filename: str, record_offset: int, re
 
 def extract_html(content: str, trafilatura_config: ConfigParser) -> str:
     """Extracts structured data from HTML content using Trafilatura."""
+    logger.info(f"Extracting HTML...{content[:10]}")
     return trafilatura.extract(
         content,
         no_fallback=True,
@@ -31,3 +38,31 @@ def extract_html(content: str, trafilatura_config: ConfigParser) -> str:
         deduplicate=True,
         config=trafilatura_config,
     )
+
+def validate_commoncrawl_index_filters(filters: List) -> List:
+    """Validates that the filters are correct."""
+    logger.info(f"Validating filters: {filters}")
+    filters = [(d["field"], d["operator"], d["value"]) for d in filters]
+
+    try:
+        if not isinstance(filters, list):
+            msg = "filters must be a list"
+            raise TypeError(msg)
+
+        for filter_tuple in filters:
+            if not isinstance(filter_tuple, tuple):
+                msg = "filters must be a list of tuples"
+                raise TypeError(msg)
+
+            field, operator, value = filter_tuple
+            if field not in VALID_COMMONCRAWL_INDEX_FILTERS:
+                msg = f"Invalid field: {field} in filter expression: {filter_tuple}"
+                raise ValueError(msg)
+
+            if operator not in VALID_OPERATORS:
+                msg = f"Invalid operator: {operator} in filter expression: {filter_tuple}"
+                raise ValueError(msg)
+
+        return filters
+    except Exception as e:
+        raise e
