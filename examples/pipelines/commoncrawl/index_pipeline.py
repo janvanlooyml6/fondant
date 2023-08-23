c86f37a3ac7753d16519fff181d176df53e5e6e1
Sharon Grundmann <sharon.grundmann@ml6.eu>
2023-08-23 18:08:44 +0200

    Add pipeline for testing cc index filtering

---
+++ examples/pipelines/commoncrawl/index_pipeline.py
@@ -0,0 +1,32 @@
+import logging
+import sys
+
+sys.path.append("../../..")
+from fondant.pipeline import ComponentOp, Pipeline
+
+logger = logging.getLogger(__name__)
+
+# initialize pipeline
+commoncrawl_pipeline = Pipeline(
+    pipeline_name="commoncrawl_pipeline",
+    base_path="/Users/sharongrundmann/Projects/fondant/fondant_artifacts",
+    pipeline_description="A pipeline for downloading Common crawl files.",
+)
+
+# define ops
+common_crawl_download_op = ComponentOp(
+    # component_dir="components/common_crawl_download",
+    component_dir="/Users/sharongrundmann/Projects/fondant/components/common_crawl_download",
+    arguments={
+        "filters": [
+            {"field": "crawl", "operator": "==", "value": "CC-MAIN-2023-14"},
+            {"field": "subset", "operator": "==", "value": "warc"},
+            {"field": "content_mime_type", "operator": "==", "value": "text/html"},
+        ],
+        "extract_plain_text": True,
+        "n_records_to_download": 10,
+    },
+)
+
+# add ops to pipeline
+commoncrawl_pipeline.add_op(common_crawl_download_op)
